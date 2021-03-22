85c9a4471d8d49e285643a26f618a98608064f7d
grzegorzpryjma99 <62578864+grzegorzpryjma99@users.noreply.github.com>
2021-03-22 23:34:42 +0100

    Create backend

---
+++ src/main/java/com/cinea/demo/CineaApplication.java
@@ -0,0 +1,13 @@
+package com.cinea.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cinea/demo/CineaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinea.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
